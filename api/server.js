a1e72d28d4f0ce371c6e932e14736fd0fe46fe99
borjasolerr <borjasolrey@gmail.com>
2019-04-11 16:52:50 +0200

    Added check in API endpoints

--- api/server.js
+++ api/server.js
@@ -4,4 +4,29 @@ const app = express();
 
 app.use(express.json());
 
+app.post('/', async (req, res) => {
+  if (req.body.name) {
+    try {
+      res.status(201).json({ message: 'New resource created' });
+    } catch (error) {
+      res.status(500).json({ message: 'Server error', error });
+    }
+  } else {
+    res.status(400).json({ message: 'Please enter name.' });
+  }
+});
+
+app.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  if (id) {
+    try {
+      res.status(200).json({ message: 'Resource deleted' });
+    } catch (error) {
+      res.status(500).json({ message: 'Server error', error });
+    }
+  } else {
+    res.status(404).json({ message: 'Resource with this ID does not exist' });
+  }
+});
+
 module.exports = app;
--- api/server.test.js
+++ api/server.test.js
@@ -6,14 +6,37 @@ describe('Express app', () => {
     it('will create new resource', () => {
       const newResourceResponse = { message: 'New resource created' };
       return request(app)
-        .post('/', { name: 'Bobas' })
+        .post('/')
+        .send({ name: 'Bobas' })
         .expect(newResourceResponse);
     });
 
     it('will response with 201 status code', () => {
       return request(app)
-        .post('/', { name: 'Bobas' })
+        .post('/')
+        .send({ name: 'Bobas' })
         .expect(201);
+    });
+  });
+
+  describe('Delete a resource', () => {
+    it('will delete resource', () => {
+      const newResourceResponse = { message: 'Resource deleted' };
+      return request(app)
+        .delete('/1')
+        .expect(newResourceResponse);
+    });
+
+    it('will response with 200 status code', () => {
+      return request(app)
+        .delete('/1')
+        .expect(200);
+    });
+
+    it('will fail with 404 status code on missing ID in params', () => {
+      return request(app)
+        .delete('/')
+        .expect(404);
     });
   });
 });
