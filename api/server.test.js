1c6872b5883489627c7b4f869c8d3c730a5e0f2a
borjasolerr <borjasolrey@gmail.com>
2019-04-11 16:51:51 +0200

    Added tests for post

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,19 @@
 const app = require('./server');
 const request = require('supertest');
 
-describe('Express app', () => {});
+describe('Express app', () => {
+  describe('Create new resource', () => {
+    it('will create new resource', () => {
+      const newResourceResponse = { message: 'New resource created' };
+      return request(app)
+        .post('/', { name: 'Bobas' })
+        .expect(newResourceResponse);
+    });
+
+    it('will response with 201 status code', () => {
+      return request(app)
+        .post('/', { name: 'Bobas' })
+        .expect(201);
+    });
+  });
+});
